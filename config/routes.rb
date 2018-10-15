50c8960fa4e014d8ccf009fd709bc36cd9dc4470
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-10-15 09:44:44 +0900

    Comment
    中途半端ですがコメント機能です

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'prototypes#index'
 
-<<<<<<< HEAD
   resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]do
   resources :comments, only: [:create]
   end
-=======
-  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
->>>>>>> master
   resources :users, only: [:show, :edit, :update]
 end
