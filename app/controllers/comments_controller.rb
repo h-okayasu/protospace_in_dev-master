9043202b026dfe106cd3f35c8f2ef13a015fea72
kawashima-mikiya <kawashimamikiya@kawashimatadashikinarinoMacBook-puro.local>
2018-10-17 10:09:27 +0900

    Comment
    途中だよ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,13 @@
 class CommentsController < ApplicationController
   def create
   	@comment = Comment.create(text: comment_params[:text], prototype_id: comment_params[:prototype_id], user_id: current_user.id)
-  	redirect_to :root
+  	redirect_to "/prototypes/#{@comment.prototype.id}"
+  end
+
+  def destroy
+  	comment = Commment.find(params[:id])
+      @comment.destroy
+      redirect_to :root, notice: '削除しました'
   end
 
   private 
